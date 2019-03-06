1a9a2307a08fb290dac2a21e174b8574ed1717a7
Hamda binte Ajmal <hamda.binte.ajmal@gmail.com>
2019-03-06 11:03:55 +0000

    apply different constraint based algorithms in bnlearn and compare the results.

---
+++ BNlearnConstraintBasedAlgosComparison.R
@@ -0,0 +1,115 @@
+## Same problem try to solve by different algos  in bnlearn
+##Comparison with bnlearn constraint base algos
+
+bl <- CreateBlackList(df)
+
+## constrained Based
+pc_0.05 <- pc.stable(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl,
+          alpha = 0.05, B = NULL, max.sx = NULL, debug = FALSE, undirected = FALSE)
+pc_0.01 <- pc.stable(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl,
+                     alpha = 0.01, B = NULL, max.sx = NULL, debug = FALSE, undirected = FALSE)
+pc_0.1 <- pc.stable(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl,
+                     alpha = 0.1, B = NULL, max.sx = NULL, debug = FALSE, undirected = FALSE)
+
+gs_0.05 <- gs(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, 
+   alpha = 0.05, debug = FALSE, optimized = FALSE, strict = FALSE, undirected = FALSE)
+
+
+gs_0.01 <- gs(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, 
+              alpha = 0.01, debug = FALSE, optimized = FALSE, strict = FALSE, undirected = FALSE)
+gs_0.1 <- gs(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, 
+              alpha = 0.1, debug = FALSE, optimized = FALSE, strict = FALSE, undirected = FALSE)
+
+
+iamb_0.05 <- iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+     alpha = 0.05, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+     strict = FALSE, undirected = FALSE)
+iamb_0.01 <- iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                  alpha = 0.01, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                  strict = FALSE, undirected = FALSE)
+iamb_0.1 <- iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                  alpha = 0.1, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                  strict = FALSE, undirected = FALSE)
+iamb_0.2 <- iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                 alpha = 0.2, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                 strict = FALSE, undirected = FALSE)
+
+fiamb_0.05 <- fast.iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+          alpha = 0.05, B = NULL, max.sx = NULL, debug = TRUE, optimized = FALSE,
+          strict = FALSE, undirected = FALSE)
+fiamb_0.01 <- fast.iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                        alpha = 0.01, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                        strict = FALSE, undirected = FALSE)
+fiamb_0.1 <- fast.iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                        alpha = 0.1, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                        strict = FALSE, undirected = FALSE)
+
+iiamb_0.05 <- inter.iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+           alpha = 0.05, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+           strict = FALSE, undirected = FALSE)
+iiamb_0.01 <- inter.iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                         alpha = 0.01, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                         strict = FALSE, undirected = FALSE)
+iiamb_0.1 <- inter.iamb(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                         alpha = 0.1, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                         strict = FALSE, undirected = FALSE)
+
+mmpc_0.05 <- mmpc(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+     alpha = 0.05, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+     strict = FALSE, undirected = FALSE)
+
+mmpc_0.01 <- mmpc(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                  alpha = 0.01, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                  strict = FALSE, undirected = FALSE)
+
+mmpc_0.1 <- mmpc(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                  alpha = 0.1, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                  strict = FALSE, undirected = FALSE)
+
+hiton_0.05 <- si.hiton.pc(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+            alpha = 0.05, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+            strict = FALSE, undirected = FALSE)
+hiton_0.01 <- si.hiton.pc(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                          alpha = 0.01, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                          strict = FALSE, undirected = FALSE)
+hiton_0.1 <- si.hiton.pc(x = dataS, cluster = NULL, whitelist = NULL, blacklist = bl, test = NULL,
+                          alpha = 0.1, B = NULL, max.sx = NULL, debug = FALSE, optimized = FALSE,
+                          strict = FALSE, undirected = FALSE)
+
+
+
+DBNList <- list("pc_0.01" = pc_0.01, "pc_0.05" = pc_0.05, "pc_0.1" = pc_0.1,
+                "gs_0.01" = gs_0.01, "gs_0.05" = gs_0.05, "gs_0.1" = gs_0.1,
+                "iamb_0.01" = iamb_0.01, "iamb_0.05" = iamb_0.05, "iamb_0.1" = iamb_0.1,
+                "iiamb_0.01" = iiamb_0.01, "iiamb_0.05" = iiamb_0.05, "iiamb_0.1" = iiamb_0.1,
+                "mmpc_0.01" = mmpc_0.01, "mmpc_0.05" = mmpc_0.05, "mmpc_0.1" = mmpc_0.1,
+                "real" = realDBN)
+GenerateResults(dataS, DBNList, realDBN)
+
+
+## Since in our data edges can only move forwards, so if there is any undirected edges, convert them into
+## directed edges as arcs can only go from previous time slice to next time slice.
+OrientEdges <- function (bn)
+{
+  edge.set <- arcs(bn)
+  bn_directed <- empty.graph(nodes = nodes(bn))
+  for (i in 1:nrow(edge.set))
+  {
+    edge <- edge.set[i,]
+    if(grepl("_", edge[1]))
+    {
+      to = edge[1]
+      from = edge[2]
+    }
+    else
+    {
+      to = edge[2]
+      from = edge[1]
+      
+    }
+    bn_directed <- set.arc(bn_directed, to = to, from = from)
+    
+  }
+  return (bn_directed)
+  
+}
