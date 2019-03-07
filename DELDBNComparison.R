36b151fdbfd8697efea292bd8bf4fae99a767991
Hamda binte Ajmal <hamda.binte.ajmal@gmail.com>
2019-03-07 13:32:44 +0000

    DELDBN

---
+++ DELDBNComparison.R
@@ -0,0 +1,44 @@
+## DELDBN
+
+
+library(bnlearn)
+data= dataset$data#ead.table("data_1.txt")
+data2= dataS
+
+DELDBN <- function(dataS, alpha)
+{
+  all_parents <- names(dataS)[which(!grepl("_", names(dataS)))]
+  all_targets <- names(dataS)[which(grepl("_", names(dataS)))]
+
+  bns <- lapply(all_targets, function(target)
+  {
+    data_temp <- dataS[, c(all_parents, target )]
+    learn.mb(data_temp, target, alpha = alpha, "gs")
+  })
+
+  bn <- empty.graph(names(dataS))
+  for (i in 1:length(bns))
+  {
+    target <- paste0("V", i ,"_1")
+    parents <- bns[[i]]
+    if (length(parents) > 0)
+    {
+      for (p in 1:length(parents))
+      {
+        bn <- set.arc(bn, from = parents[p], to = target)
+      }
+    }
+    
+    
+  }
+  return (bn)
+}
+delDBN_0.001 <- DELDBN(dataS,0.001)
+delDBN_0.005 <- DELDBN(dataS,0.005)
+delDBN_0.01 <- DELDBN(dataS,0.01)
+delDBN_0.05 <- DELDBN(dataS, 0.05)
+delDBN_0.1 <- DELDBN(dataS, 0.1)
+
+DBNList <- list("DELDBN" = delDBN_0.001, "DELDBN" = delDBN_0.005, "DELDBN" = delDBN_0.01 ,"DELDBN" = delDBN_0.05, "DELDBN" = delDBN_0.1, "real" = realDBN)
+GenerateResults(dataS, DBNList, realDBN)
+
